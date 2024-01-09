a7046482d5b71c2c0eb7c3815f8067c5a4664a63
tanish105 <nagranitanish@gmail.com>
2024-01-09 12:05:47 +0530

    created basic outline for otp flow

--- lib/UI/screens/Login Screen/controller/loginController.dart
+++ lib/UI/screens/Login Screen/controller/loginController.dart
@@ -1,11 +1,74 @@
+import 'dart:convert';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:rttv/UI/screens/Login%20Screen/model/loginResponseModel.dart';
+import 'package:rttv/UI/screens/Login%20Screen/model/userModel.dart';
+import 'package:http/http.dart' as http;
+import 'package:rttv/utility/PostResponse/PostResponseType.dart';
+import 'package:rttv/utility/strings.dart';
 
 class LoginController extends GetxController {
   final emailController = TextEditingController().obs;
   final passwordController = TextEditingController().obs;
+  final phoneNoController = TextEditingController().obs;
+  final baseURL = dotenv.env['BASE_URL'];
+  RxBool isValid = false.obs;
 
-  void loginApi(String email, String password){
+  bool validEmail = false;
+  bool validPhoneNo = false;
+  bool validPassword = false;
 
+  Future<PostResponseType> loginApi(userModel user) async{
+    final url = Uri.parse(baseURL! + "login");
+    print(user.toJson());
+    try{
+      var res = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: jsonEncode(user.toJson()),
+      );
+      Map<String, dynamic> body = jsonDecode(res.body);
+      print(res.statusCode);
+      print(res.body);
+      loginResponseModel responseModel = loginResponseModel(body);
+      if(responseModel.code==1){
+        return PostResponseType(postResponseEnum: PostResponseEnum.success, message: SignUpSuccess);
+      }else{
+        if(res.statusCode==400){
+          return PostResponseType(postResponseEnum: PostResponseEnum.error, message: responseModel.message!);
+        }else{
+          return PostResponseType(postResponseEnum: PostResponseEnum.failed, message: responseModel.message!);
+        }
+      }
+    }catch(err){
+      print(err);
+      return PostResponseType(postResponseEnum: PostResponseEnum.failed, message: CheckInternet);
+    }
+  }
+
+  void test(){
+    print("${baseURL}login");
+  }
+
+  void checkEmail(){
+    validEmail = EmailValidator.validate(emailController.value.text);
+    checkContraints();
+  }
+
+  void checkPhoneNumber() {
+    String phoneNumber = phoneNoController.value.text;
+    validPhoneNo = phoneNumber.length == 13 && phoneNumber.startsWith("+91");
+    checkContraints();
+  }
+
+
+  void checkContraints(){
+    if(validEmail && validPhoneNo && validPassword){
+      isValid.value = true;
+    }
   }
 }
--- lib/UI/screens/Login Screen/view/login_screen.dart
+++ lib/UI/screens/Login Screen/view/login_screen.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rttv/UI/screens/Login%20Screen/controller/loginController.dart';
+import 'package:rttv/UI/screens/Login%20Screen/model/userModel.dart';
 import 'package:rttv/resources/routes/routes_name.dart';
+import 'package:rttv/utility/PostResponse/PostResponseType.dart';
 import 'package:rttv/utility/numbers.dart';
 import 'package:rttv/utility/strings.dart';
 
@@ -55,114 +57,153 @@ class _LoginPageState extends State<LoginPage> {
                   Expanded(
                     flex: 15,
                     child: Container(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(forty),
-                            child: Image.asset(
-                              RTTVLOGO,
-                              scale: eight,
+                      child: Obx(
+                        () => Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(forty),
+                              child: Image.asset(
+                                RTTVLOGO,
+                                scale: eight,
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: thirty),
-                            child: TextFieldInput(
-                                textEditingController: loginController.emailController.value,
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: thirty),
+                              child: TextFieldInput(
+                                textEditingController:
+                                    loginController.emailController.value,
                                 hintText: EMAIL,
-                                 textInputType: TextInputType.emailAddress,
+                                textInputType: TextInputType.emailAddress,
                                 hideText: false,
-                                onChange: (str){
-
-                                },),
-                          ),
-                          const SizedBox(
-                            height: eighteen,
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: thirty),
-                            child: TextFieldInput(
-                                textEditingController: loginController.passwordController.value,
+                                onChange: (str) {},
+                              ),
+                            ),
+                            const SizedBox(
+                              height: eighteen,
+                            ),
+                            Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: thirty),
+                                  child: TextFieldInput(
+                                    textEditingController: loginController
+                                        .phoneNoController.value,
+                                    hintText: PHONE_NUMBER,
+                                    textInputType: TextInputType.emailAddress,
+                                    hideText: false,
+                                    onChange: (str) {
+                                      loginController.checkPhoneNumber();
+                                    },
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: eighteen,
+                                ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: thirty),
+                              child: TextFieldInput(
+                                textEditingController:
+                                    loginController.passwordController.value,
                                 hintText: PASSWORD,
                                 textInputType: TextInputType.visiblePassword,
                                 hideText: true,
-                                onChange: (str){
-                                  
-                                },),
-                          ),
-                          const SizedBox(
-                            height: thirtysix,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: onehundredtwenty),
-                            child: GestureDetector(
-                              // Inside GestureDetector for Login button
-                              onTap: () {
-                                loginController.loginApi(
-                                  loginController.emailController.value.text,
-                                  loginController.passwordController.value.text,
-                                );
-                                Get.toNamed(RouteName.otpScreen);
-                              },
-                              //Call Login function
-                              child: Container(
-                                width: double.infinity,
-                                alignment: Alignment.center,
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: one),
-                                decoration: const ShapeDecoration(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(four),
+                                onChange: (str) {},
+                              ),
+                            ),
+                            const SizedBox(
+                              height: thirtysix,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: onehundredtwenty),
+                              child: GestureDetector(
+                                // Inside GestureDetector for Login button
+                                onTap: loginController.isValid.value
+                                    ? () async {
+                                        loginController.test();
+                                        PostResponseType result =
+                                            await loginController.loginApi(
+                                          userModel(
+                                              phone_number: loginController
+                                                  .phoneNoController.value.text,
+                                              email: loginController
+                                                  .emailController.value.text,
+                                              password: loginController
+                                                  .passwordController.value.text),
+                                        );
+                                        if (result.postResponseEnum ==
+                                            PostResponseEnum.success) {
+                                          Get.snackbar(SUCCESS, result.message,
+                                              snackPosition:
+                                                  SnackPosition.BOTTOM);
+                                          Get.toNamed(RouteName.otpScreen,
+                                              arguments: loginController
+                                                  .phoneNoController.value.text);
+                                        } else {
+                                          Get.snackbar(ERROR, result.message,
+                                              snackPosition:
+                                                  SnackPosition.BOTTOM);
+                                        }
+                                      }
+                                    : () {},
+                                child: Container(
+                                  width: double.infinity,
+                                  alignment: Alignment.center,
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: one),
+                                  decoration: const ShapeDecoration(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(four),
+                                        ),
                                       ),
+                                      color:  Colors.red),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(eight),
+                                    child: Text(
+                                      LOGIN,
+                                      style: TextStyle(
+                                          fontSize: twentyfive,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
                                     ),
-                                    color: Colors.red),
-                                child: const Padding(
-                                  padding: EdgeInsets.all(eight),
-                                  child: Text(
-                                    LOGIN,
-                                    style: TextStyle(
-                                        fontSize: twentyfive,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: forty,
-                          ),
-                          FittedBox(
-                            fit: BoxFit.fitHeight,
-                            child: Row(
-                              children: [
-                                const Text(
-                                  NEWUSER,
-                                  style: TextStyle(
-                                      fontSize: twenty, color: Colors.white),
-                                ),
-                                const SizedBox(
-                                  width: ten,
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    Get.toNamed(RouteName.signUpScreen);
-                                  },
-                                  child: const Text(
-                                    SIGNUP,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: twenty,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                              ],
+                            const SizedBox(
+                              height: forty,
                             ),
-                          ),
-                        ],
+                            FittedBox(
+                              fit: BoxFit.fitHeight,
+                              child: Row(
+                                children: [
+                                  const Text(
+                                    NEWUSER,
+                                    style: TextStyle(
+                                        fontSize: twenty, color: Colors.white),
+                                  ),
+                                  const SizedBox(
+                                    width: ten,
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      Get.toNamed(RouteName.signUpScreen);
+                                    },
+                                    child: const Text(
+                                      SIGNUP,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: twenty,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
