cefa57e8b9d92580a4365f12cfbf575e9fd410a8
tanish105 <nagranitanish@gmail.com>
2024-01-17 22:03:27 +0530

    otp autofill working

--- lib/UI/screens/OTP_Screen/view/otpScreen.dart
+++ lib/UI/screens/OTP_Screen/view/otpScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rttv/UI/screens/OTP_Screen/controller/otpController.dart';
@@ -8,6 +7,8 @@ import 'package:rttv/utility/PostResponse/PostResponseType.dart';
 import 'package:rttv/utility/numbers.dart';
 import 'package:rttv/utility/strings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:android_sms_retriever/android_sms_retriever.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class OtpScreen extends StatefulWidget {
   const OtpScreen({Key? key}) : super(key: key);
@@ -16,11 +17,71 @@ class OtpScreen extends StatefulWidget {
   State<OtpScreen> createState() => _OtpScreenState();
 }
 
-class _OtpScreenState extends State<OtpScreen> {
+class _OtpScreenState extends State<OtpScreen>
+    with SingleTickerProviderStateMixin {
   final OtpController otpController = Get.put(OtpController());
-  late String otp = '';
+  late String? otp = '';
   final String phoneNumber = Get.arguments?['phoneNumber'] ?? '';
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late TextEditingController _otpController;
+
+  @override
+  void initState() {
+    super.initState();
+    _otpController = TextEditingController();
+    _startSmsListener();
+  }
+
+  void _startSmsListener() async {
+    try {
+      AndroidSmsRetriever.getAppSignature().then((value) {
+        setState(() {
+          String _applicationSignature = value ?? 'Signature Not Found';
+          print("App Signature : $_applicationSignature");
+        });
+      });
+
+      AndroidSmsRetriever.listenForOneTimeConsent().then((value) async {
+        setState(() {
+          final intRegex = RegExp(r'\d+', multiLine: true);
+          final code = intRegex
+              .allMatches(value ?? 'Phone Number Not Found')
+              .first
+              .group(0);
+          print(code);
+          String _smsCode = code ?? '12345';
+          otp = _smsCode;
+        });
+        await _handleOtpVerification();
+      }).timeout(Duration(seconds: 30));
+    } catch (e) {
+      print('Error listening for SMS: $e');
+    }
+  }
+
+  Future<void> _handleOtpVerification() async {
+    if (otp != null && otp!.length == 6) {
+      final SharedPreferences prefs = await _prefs;
+      PostResponseType result =
+          await otpController.verifyOTP(phoneNumber, otp!);
+      if (result.postResponseEnum == PostResponseEnum.success) {
+        bool tokenSaved = await prefs.setString("token", result.data!);
+        if (tokenSaved) {
+          Get.toNamed(RouteName.homeScreen, arguments: {'token': result.data});
+        }
+      } else {
+        Get.snackbar(ERROR, result.message,
+            snackPosition: SnackPosition.BOTTOM);
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _otpController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,9 +89,10 @@ class _OtpScreenState extends State<OtpScreen> {
         title: Text(
           VERIFY_OTP,
           style: GoogleFonts.ptSerif(
-              fontSize: twentyfive,
-              color: Colors.black,
-              fontWeight: FontWeight.w600),
+            fontSize: twentyfive,
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
         ),
         leading: IconButton(
           onPressed: () {
@@ -48,24 +110,25 @@ class _OtpScreenState extends State<OtpScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Image.asset(
-                    'assets/otp_image.jpg', // Replace with your image path
-                    height: 350, // Adjust these factors as needed
-                    width: 350 // Set width as per your requirement
-                    ),
+                  'assets/otp_image.jpg', // Replace with your image path
+                  height: 350, // Adjust these factors as needed
+                  width: 350, // Set width as per your requirement
+                ),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: OtpTextField(
-                    numberOfFields: 6,
-                    fieldWidth: sixty / 1.5,
-                    borderColor: Colors.red,
-                    showFieldAsBox: true,
-                    onCodeChanged: (code) {
+                  child: PinCodeTextField(
+                    appContext: context,
+                    length: 6,
+                    onChanged: (code) {
                       otp = code;
                     },
-                    onSubmit: (code) {
-                      print(code);
-                      otp = code;
-                    },
+                    pinTheme: PinTheme(
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(5),
+                      fieldHeight: 50,
+                      fieldWidth: 40,
+                      activeFillColor: Colors.white,
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -81,42 +144,28 @@ class _OtpScreenState extends State<OtpScreen> {
               padding: EdgeInsets.all(twenty),
               child: InkWell(
                 child: GestureDetector(
-                  onTap: () async {
-                    print(phoneNumber);
-                    final SharedPreferences prefs = await _prefs;
-                    PostResponseType result =
-                        await otpController.verifyOTP(phoneNumber, otp);
-                    if (result.postResponseEnum == PostResponseEnum.success) {
-                      bool tokenSaved =
-                          await prefs.setString("token", result.data!);
-                      if (tokenSaved) {
-                        Get.toNamed(RouteName.homeScreen,
-                            arguments: {'token': result.data});
-                      }
-                    } else {
-                      Get.snackbar(ERROR, result.message,
-                          snackPosition: SnackPosition.BOTTOM);
-                    }
-                  },
+                  onTap: _handleOtpVerification,
                   child: Container(
                     width: double.infinity,
                     alignment: Alignment.center,
                     padding: const EdgeInsets.symmetric(vertical: one),
                     decoration: const ShapeDecoration(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(four),
-                          ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(four),
                         ),
-                        color: Colors.red),
+                      ),
+                      color: Colors.red,
+                    ),
                     child: Padding(
                       padding: const EdgeInsets.all(eight),
                       child: Text(
                         SUBMIT_OTP,
                         style: GoogleFonts.ptSerif(
-                            fontSize: twentyfive,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
+                          fontSize: twentyfive,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
